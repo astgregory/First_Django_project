a9a9edc70969c153dc5750cd80bd08691876034c
Mr. Gregory <astgregory@bk.ru>
2023-07-17 20:39:25 +0300

    Part 6. Create view file and create templates

--- project/orders/views.py
+++ project/orders/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
 
-from orders.models import SalesOrder
-
-
-def orders_page(request):
-    return render(request, 'index.html', {'orders': SalesOrder.objects.all()})
-
+# Create your views here.
