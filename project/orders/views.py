d02d0d086fd7e2f55701deb12be2ce0eb92e693c
Mr. Gregory <astgregory@bk.ru>
2023-07-18 21:12:21 +0300

    Part 7. API view on Django

--- project/orders/views.py
+++ project/orders/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from orders.models import SalesOrder
+from orders.serializers import OrderSerializer
+
+
+def orders_page(request):
+    return render(request, 'index.html', {'orders': SalesOrder.objects.all()})
+
+class OrderView(ModelViewSet):
+    queryset = SalesOrder.objects.all()
+    serializer_class = OrderSerializer
+
